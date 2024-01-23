7e33404c5c4ade682fc92934bda0c1bbc137f7ac
Thiago Luiz Silva - Desenvolvimento <23242@cenl.cal>
2024-01-23 18:32:40 -0300

    initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <footer>
-      <div>Footer</div>
+    <footer className="h-20 bg-[#2D2D2D] flex items-center">
+      <div className="flex justify-between items-center px-[60px] w-full">
+        <p className="text-white text-sm">
+          © 2023 Animas Landing Page Ui Kit.{" "}
+        </p>
+        <div className="flex gap-4">
+          <Link href="#">
+            <Image src="/Social Icons.svg" width={32} height={32} alt="" />
+          </Link>
+          <Link href="#">
+            <Image src="/Social Icons (1).svg" width={32} height={32} alt="" />
+          </Link>
+          <Link href="#">
+            <Image src="/Social Icons (2).svg" width={32} height={32} alt="" />
+          </Link>
+          <Link href="#">
+            <Image src="/Social Icons (3).svg" width={32} height={32} alt="" />
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 }
--- src/components/Metrics.tsx
+++ src/components/Metrics.tsx
@@ -1,14 +1,34 @@
 export default function Metrics() {
   return (
-    <section>
-      <div className="text-center">
-        <h3 className="text-[38px] font-bold -tracking-tight">
-          Our Metrics Tell the Story
-        </h3>
-        <p>
-          Our metrics component gives you the inside scoop on your success and
-          helps you stay on top of your game in style.
-        </p>
+    <section className="my-[100px]">
+      <div className="flex flex-col gap-[60px]">
+        <div className="text-center">
+          <h3 className="text-[38px] font-bold -tracking-tight">
+            Our Metrics Tell the Story
+          </h3>
+          <p>
+            Our metrics component gives you the inside scoop on your success and
+            helps you stay on top of your game in style.
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <div className="">
+            <p className="font-bold text-[38px]">10k+</p>
+            <p>Website launched</p>
+          </div>
+          <div className="">
+            <p className="font-bold text-[38px]">931k+</p>
+            <p>Projects created with Anima</p>
+          </div>
+          <div className="">
+            <p className="font-bold text-[38px]">240k+</p>
+            <p>New users joined Anima</p>
+          </div>
+          <div className="">
+            <p className="font-bold text-[38px]">12k+</p>
+            <p>Teams used Anima</p>
+          </div>
+        </div>
       </div>
     </section>
   );
